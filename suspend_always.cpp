d7334bb1660511571314bc8fe8f839b289c239d7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-02-17 16:44:22 +0300

    Update suspend_always.cpp

--- suspend_always.cpp
+++ suspend_always.cpp
@@ -1,1 +1,6 @@
-soon
+
+struct suspend_always {
+    constexpr bool await_ready() const noexcept { return false; }
+    constexpr void await_suspend(std::coroutine_handle<>) const noexcept {}
+    constexpr void await_resume() const noexcept {}
+}
