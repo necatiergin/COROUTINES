4f99dc6999c2a57aca321d7e279836b1289c125d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-03-18 17:18:08 +0300

    Create crtn_19.cpp

---
+++ crtn_19.cpp
@@ -0,0 +1,18 @@
+#include <generator> //C++23
+#include <utility>
+
+
+std::generator<int> fib(int n) 
+{
+    int x{ 0 }, y{ 1 };
+
+    while (n--)
+         co_yield std::exchange(x, std::exchange(y, x + y));
+}
+
+int main() 
+{
+    for (int i : fib(10)) {
+        std::cout << i;
+    }
+}
